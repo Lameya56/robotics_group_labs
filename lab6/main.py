375b4c8352657592782179b7d8ad3a46719ac3a5
Patrick Lin <patrickyan51418@gmail.com>
2025-11-28 15:38:27 -0500

    Completed Task 2 and 3

--- lab6/main.py
+++ lab6/main.py
@@ -24,6 +24,43 @@ def main():
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
         cv2.imshow("HSV", hsv)
 
+        lower1 = np.array([20, 100, 100])
+        upper1 = np.array([30, 255, 255])
+        mask1 = cv2.inRange(hsv, lower1, upper1)
+
+        lower2 = np.array([20, 100, 100])
+        upper2 = np.array([30, 255, 255])
+        mask2 = cv2.inRange(hsv, lower2, upper2)
+
+        mask = mask1 | mask2
+
+        kernel = np.ones((5, 5), np.uint8)
+        mask_clean = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+        mask_clean = cv2.morphologyEx(mask_clean, cv2.MORPH_CLOSE, kernel)
+        contours, hierarchy = cv2.findContours(mask_clean, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        cv2.imshow("Mask", mask)
+
+        result = cv2.bitwise_and(frame, frame, mask=mask)
+        cv2.imshow("Segmented Object", result)
+
+         if contours:
+            largest = max(contours, key=cv2.contourArea)
+            M = cv2.moments(largest)
+            if M["m00"] != 0:
+                cx = int(M["m10"] / M["m00"])
+                cy = int(M["m01"] / M["m00"])
+            else:
+                cx, cy = None, None
+
+            # Create a copy for drawing
+            tracked_frame = frame.copy()
+            cv2.drawContours(tracked_frame, [largest], -1, (0, 255, 0), 2)
+
+            if cx and cy:
+                cv2.circle(tracked_frame, (cx, cy), 5, (0, 0, 255), -1)
+            cv2.imshow("Tracked Object", tracked_frame)
+
         # Exit on 'q' key
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
